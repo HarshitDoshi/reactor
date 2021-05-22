ddb017506f529ffd4353522482b2a35558a21059
Codespaces <codespaces@github.com>
2021-05-22 20:28:27 +0000

    Codespaces

---
+++ templates-app/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <header>
+      <div className="flex flex-wrap flex-col md:flex-row">
+        <a href="shunyaek.se" className="flex title-font items-center my-2 md:my-0">
+          <span className="text-xl font-bold mx-4">BrandName</span>
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default App;
